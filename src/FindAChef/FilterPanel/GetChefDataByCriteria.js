1fc0b58e408c53c6b81a9f0ae652011b15f40708
mamathaguntu <mamathaguntu@gmail.com>
2020-11-24 00:40:57 -0800

    update getChefDataByCriteria

--- src/FindAChef/FilterPanel/GetChefDataByCriteria.js
+++ src/FindAChef/FilterPanel/GetChefDataByCriteria.js
@@ -1,24 +1,21 @@
 import axios from "axios";
 
-const fieldValues = new Map([
-  // ['Experience', '12'], 
-  ['Rating', '3'],
-  // ['Cuisine', [{ "S" : 'North-Indian'}]], 
-  ['Cuisine', 'North-Indian'],
-  ['Price', ['0', '21']],
-  // ,
-
-  ['Date', "11/23/2020"],
-  ['Slots', ['Lunch', 'Dinner']]
-]);
-
 const GetChefDataByCriteria = (event) => {
-    return new Promise((resolve, reject) => {
-        axios.get("https://79p73jdiu0.execute-api.us-east-1.amazonaws.com/dev/chefdata", { params: fieldValues })
+  return new Promise((resolve, reject) => {
+    axios.put("https://8pw4ve3xvb.execute-api.us-east-1.amazonaws.com/dev",
+      {
+        // fieldValues,
+        Rating: '3', //change to Rating : event.target.Rating or pass values to the function and use the same.
+        Cuisine: 'North-Indian',
+        Price: ['0', '21'],
+        Date: '11/23/2020',
+        Slots: ['Lunch', 'Dinner'],
+        Experience: ''
+      })
       .then(function (response) {
         const filteredChefData = [];
         if (response.data.length > 0) {
-          response.data.forEach(function(element) {
+          response.data.forEach(function (element) {
             filteredChefData.push({
               Email: element.Email.S,
               ChefSlots: element.ChefSlots ? element.ChefSlots.M : null,
@@ -29,16 +26,16 @@ const GetChefDataByCriteria = (event) => {
               Price: element.Price.N,
               Rating: element.Rating.N,
               Zipcode: element.Zipcode.N.ChefSlots,
-              Image: 'http://d1q6lc2rasmdf5.cloudfront.net/ChefPictures/'+ element.Email.S + '.jpg'
+              Image: 'http://d1q6lc2rasmdf5.cloudfront.net/ChefPictures/' + element.Email.S + '.jpg'
             });
           });
           console.log("response.................x", response);
-          resolve({filteredChefData});
+          resolve({ filteredChefData });
         }
       })
       .catch(function (error) {
         reject(error);
       });
-    });
+  });
 }
 export default GetChefDataByCriteria;
