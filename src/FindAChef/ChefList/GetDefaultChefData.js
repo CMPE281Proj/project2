5fbbb3b72156f87e3ca0331d445b11a909c532c8
mamathaguntu <mamathaguntu@gmail.com>
2020-11-29 18:23:02 -0800

    update

--- src/FindAChef/ChefList/GetDefaultChefData.js
+++ src/FindAChef/ChefList/GetDefaultChefData.js
@@ -1,12 +1,12 @@
 import axios from "axios";
 
 const GetDefaultChefData = (event) => {
-    return new Promise((resolve, reject) => {
-        axios.get("https://7w0xdfcdpl.execute-api.us-east-1.amazonaws.com/getDefaultChefData")
+  return new Promise((resolve, reject) => {
+    axios.get("https://7w0xdfcdpl.execute-api.us-east-1.amazonaws.com/getDefaultChefData")
       .then(function (response) {
         const defaultChefData = [];
         if (response.data.length > 0) {
-          response.data.forEach(function(element) {
+          response.data.forEach(function (element) {
             defaultChefData.push({
               Email: element.Email.S,
               ChefSlots: element.ChefSlots ? element.ChefSlots.M : null,
@@ -16,16 +16,16 @@ const GetDefaultChefData = (event) => {
               Name: element.Name ? element.Name.S : '',
               Price: element.Price.N,
               Rating: element.Rating.N,
-              Zipcode: element.Zipcode.N.ChefSlots,
-              Image: 'http://d1q6lc2rasmdf5.cloudfront.net/ChefPictures/ChefProfilePics/'+ element.Email.S + '.jpg'
+              Zipcode: element.Zipcode.N,
+              Image: 'http://d1q6lc2rasmdf5.cloudfront.net/ChefPictures/ChefProfilePics/' + element.Email.S + '.jpg'
             });
           });
-          resolve({defaultChefData});
+          resolve({ defaultChefData });
         }
       })
       .catch(function (error) {
         reject(error);
       });
-    });
+  });
 }
 export default GetDefaultChefData;
