2e2feb0c3b223318b9e8621db520a1c52117559a
mamathaguntu <mamathaguntu@gmail.com>
2020-12-07 21:48:49 -0800

    pool changes

--- src/aws-exports.js
+++ src/aws-exports.js
@@ -3,7 +3,7 @@
 
 const awsmobile = {
     "aws_project_region": "us-east-1",
-    "aws_cognito_identity_pool_id": "us-east-1:34e871f9-8139-42ec-9778-38971ed4273a",
+    "aws_cognito_identity_pool_id": "us-east-1:cbc93477-363a-48bd-9744-d38f018c08cd",
     "aws_cognito_region": "us-east-1",
     "aws_user_pools_id": "us-east-1_QmPlkTkyi",
     "aws_user_pools_web_client_id": "7552ihptjm82k06g2eaad8q6c1",
